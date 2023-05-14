6f102ed1d2ef0096d886699ddda0ce82a2c4f1b4
asepirfans <asepirfans.9c@gmail.com>
2023-05-14 21:03:03 +0700

    add urlencoded

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ try {
 app.use(cors())
 app.use(cookieParser());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
     res.send("API Ready To GO!");
   });
